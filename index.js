b060105bbbc04c0240edf9db622d144266403225
Eli Reiners <goelyukim@gmail.com>
2019-11-24 14:55:33 -0500

    Add function outlines
    This code acts as a basic outline for what is to come

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+function getWeather(){
+
+};
+
+function getNews(){
+
+};
+
+function displayResults(){
+
+};
+
+function renderResultsScreen() {
+
+};
+
+function handleSubmit() {
+
+};
+
+function handleSearchButtons(){
+
+};
+
+function renderSearchScreen() {
+
+};
+
+function handleHomeButton() {
+
+};
+
+function handleExploreApp() {
+    handleHomeButton();
+    handleSearchButtons();
+};
+
+$(handleExploreApp);
